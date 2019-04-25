9e27ba12689f3666338cfcd57e532afbb96c0f2a
Rain Johannes <rainjohannes@gmail.com>
2019-04-25 14:42:32 -0400

    End of lecture 4/25/19
    New features, but sending duplicate outputs. Saving first to try a new method

--- finalproject.ino
+++ finalproject.ino
@@ -1,48 +1,83 @@
 int pot = A14;
+int bendToggle = 35;
+int modToggle = 34;
 
-int lastPotVal;
-int currentPotVal;
-int mappedPot;
+int lastStretchVal;
+int currentStretchVal;
 
-int lowReadVal = 50;
+int lastBendVal;
+int currentBendVal;
+
+int lastModVal;
+int currentModVal;
+
 int highReadVal = 100;
+int lowReadVal = 50;
 
-int pitchBendHigh = 8191;
-int pitchBendLow = -8192;
+int bendHigh = 8191;
+int bendLow = -8192;
+
+int modHigh = 127;
+int modLow = 0;
 
 int smoothTime = 1;
 unsigned long lastSmoothTime = millis();
 
 void setup() {
   Serial.begin(9600);
+  pinMode(bendToggle, INPUT);
+  pinMode(modToggle, INPUT);
 
 }
 
 void loop() {
-  checkPot();
+  checkStretch();
 
 }
 
-void checkPot() {
-  lastPotVal = currentPotVal;
+void checkStretch() {
+  lastStretchVal = currentStretchVal;
+  lastBendVal = currentBendVal;
+  lastModVal = currentModVal;
 
-  //if (millis() >= lastSmoothTime + smoothTime) {
-  currentPotVal = (analogRead(pot));
+  if (millis() >= lastSmoothTime + smoothTime) {
+    if ((analogRead(pot) - lastStretchVal >= 4) || (lastStretchVal - analogRead(pot) >= 4)) {
+      currentStretchVal = (analogRead(pot));
+    }
 
-  if (currentPotVal <= lowReadVal) {
-    mappedPot = 0;
-  }
+    if (currentStretchVal <= lowReadVal) {
+      currentBendVal = 0;
+      currentModVal = 0;
+    }
 
-  else if (map(currentPotVal, lowReadVal, highReadVal, 0, pitchBendHigh) >= 0) {
-    mappedPot = map(currentPotVal, lowReadVal, highReadVal, 0, pitchBendHigh);
-  }
+    else if (currentStretchVal <= highReadVal) {
+      currentModVal = map(currentStretchVal, lowReadVal, highReadVal, modLow, modHigh);
 
-  //lastSmoothTime = millis();
+      if (digitalRead(bendToggle) == HIGH) {
+        currentBendVal = map(currentStretchVal, lowReadVal, highReadVal, 0, bendHigh);
+      }
 
+      else if (digitalRead(bendToggle) == LOW) {
+        currentBendVal = map(currentStretchVal, lowReadVal, highReadVal, 0, bendLow);
+      }
+    }
 
-  if (lastPotVal != currentPotVal) {
-    Serial.println(currentPotVal);
-    usbMIDI.sendPitchBend(mappedPot, 1);
+    if (digitalRead(modToggle) == LOW) {
+      if (lastBendVal != currentBendVal); {
+        Serial.println(currentBendVal);
+        usbMIDI.sendPitchBend(currentBendVal, 1);
+      }
+    }
+
+    else if (digitalRead(modToggle) == HIGH) {
+      if (lastModVal != currentModVal); {
+        Serial.println(currentModVal);
+        usbMIDI.sendControlChange(1, currentModVal, 1);
+      }
+    }
+
+    lastSmoothTime = millis();
+
   }
 
 }
